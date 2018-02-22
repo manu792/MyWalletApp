9bfe233da8edf231c52dd62eb94665f4f24978de
Manuel Roman <MRoman@tradestation.com>
2018-02-22 08:15:36 -0600

    add total feature for Home page

--- MyWalletApp/Controllers/HomeController.cs
+++ MyWalletApp/Controllers/HomeController.cs
@@ -9,15 +9,25 @@ namespace MyWalletApp.Controllers
 {
     public class HomeController : Controller
     {
+        private IngresoManager ingresoManager;
+        private GastoManager gastoManager;
 
         public HomeController()
         {
-            
+            ingresoManager = new IngresoManager();
+            gastoManager = new GastoManager();
         }
 
         // GET: Home
         public ActionResult Index()
         {
+            var ingresos = ingresoManager.GetAllIngresos();
+            var gastos = gastoManager.GetAllGastos();
+
+            var montoReal = ingresos.Sum(i => i.Monto) - gastos.Sum(g => g.Monto);
+
+            ViewBag.Billetera = montoReal;
+
             return View();
         }
     }
