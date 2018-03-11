375797ba5cbfa2b047b3abd28e49c27d49ebb3d8
Manuel Roman <MRoman@tradestation.com>
2018-03-11 09:07:17 -0600

    new features

---
+++ MyWalletApp/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using MyWalletApp.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWalletApp.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private ServicioManager servicioManager;
+
+        public NotificationController()
+        {
+            servicioManager = new ServicioManager();
+        }
+
+        // GET: Notification
+        public ActionResult GetNotifications()
+        {
+            var proximosPagos = servicioManager.GetServiciosWithinNextFiveDays();
+            var json = new JsonResult();
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            json.Data = proximosPagos;
+
+            return json;
+        }
+    }
+}
